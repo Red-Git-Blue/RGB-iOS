d95e96e836dfd87307f9c7a9f2e9866004ca16e8
junha <goodjunha@gmail.com>
2022-10-10 21:33:27 +0900

    🟥🟩🟦:: new RGB setting

---
+++ RGB/RGB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGB
+//
+//  Created by 박준하 on 2022/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
