b7b332c06c652aba93a8c6d4bab1ef33683365dd
junha <goodjunha@gmail.com>
2022-11-19 19:13:39 +0900

    🔫:: myRankingVIew

--- RGB/UI/Scene/Chart/Rank/MyRankView.swift
+++ RGB/UI/Scene/Chart/Rank/MyRankView.swift
@@ -31,13 +31,41 @@ class MyRankView: UIView {
         $0.backgroundColor = .gray
         $0.layer.cornerRadius = 20
     }
+    
+    private lazy var fundsLabel = UILabel().then {
+        $0.text = "자금"
+        $0.font = .systemFont(ofSize: 14.0, weight: .bold)
+        $0.textColor = .red
+    }
+    
+    private lazy var fundsRanking = UILabel().then {
+        $0.text = "132위"
+        $0.font = .systemFont(ofSize: 14.0, weight: .regular)
+        $0.textColor = .white
+    }
+    
+    private lazy var revenueLabel = UILabel().then {
+        $0.text = "수익률"
+        $0.font = .systemFont(ofSize: 14.0, weight: .bold)
+        $0.textColor = .red
+    }
+    
+    private lazy var revenueRanking = UILabel().then {
+        $0.text = "258위"
+        $0.font = .systemFont(ofSize: 14.0, weight: .regular)
+        $0.textColor = .white
+    }
 }
 
 extension MyRankView {
     func setup() {
         [
             myRankLabel,
-            myRankView
+            myRankView,
+            fundsLabel,
+            fundsRanking,
+            revenueLabel,
+            revenueRanking
         ].forEach { self.addSubview($0) }
         
         myRankLabel.snp.makeConstraints {
@@ -50,6 +78,26 @@ extension MyRankView {
             $0.leading.equalTo(myRankLabel.snp.leading)
             $0.height.equalTo(50.0)
             $0.width.equalTo(370.0)
+        }
+        
+        fundsLabel.snp.makeConstraints {
+            $0.top.equalTo(myRankView.snp.top).offset(17.0)
+            $0.leading.equalTo(myRankView.snp.leading).inset(20.0)
+        }
+        
+        fundsRanking.snp.makeConstraints {
+            $0.top.equalTo(fundsLabel.snp.top)
+            $0.leading.equalTo(fundsLabel.snp.trailing).offset(20.0)
+        }
+        
+        revenueLabel.snp.makeConstraints {
+            $0.top.equalTo(fundsRanking.snp.top)
+            $0.leading.equalTo(fundsRanking.snp.trailing).offset(80.0)
+        }
+        
+        revenueRanking.snp.makeConstraints {
+            $0.top.equalTo(revenueLabel.snp.top)
+            $0.leading.equalTo(revenueLabel.snp.trailing).offset(20.0)
         }
     }
 }
